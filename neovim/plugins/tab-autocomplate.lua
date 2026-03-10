70f68714db1ab93172dc9d81ce69fc0358845db0
blend-tea <161995054+blend-tea@users.noreply.github.com>
2026-03-10 18:04:20 +0900

    Refactor tab completion configuration in Neovim

--- neovim/plugins/tab-autocomplate.lua
+++ neovim/plugins/tab-autocomplate.lua
@@ -1,12 +1,16 @@
 return {
-  {
-    "saghen/blink.cmp",
-    opts = function(_, opts)
-      opts.keymap = {
-        preset = "super-tab",
-        ["<Tab>"] = { "select_and_accept" },
-        ["<S-Tab>"] = { "select_prev" },
-      }
-    end,
-  },
+  "saghen/blink.cmp",
+  opts = {
+    completion = {
+      -- Insert completion item on selection, don't select by default
+      list = {
+        selection = {
+          auto_insert = false
+        }
+      },
+    },
+    keymap = {
+      preset = "super-tab",
+    },
+  }
 }
