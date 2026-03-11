f38528c43b4cf675d0ec24164ab4a2456a0bbfa2
blend-tea <161995054+blend-tea@users.noreply.github.com>
2026-03-11 13:38:13 +0900

    Add keymaps to close terminals with Ctrl-/
    Added keymaps for closing terminals in terminal mode.

---
+++ neovim/config/keymaps.lua
@@ -0,0 +1,19 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- Close terminal with <C-/> in terminal mode
+-- (Snacks-managed terminals are hidden; others close the window)
+local function close_terminal()
+  local buf = vim.api.nvim_get_current_buf()
+  for _, terminal in ipairs(Snacks.terminal.list()) do
+    if terminal.buf == buf then
+      terminal:hide()
+      return
+    end
+  end
+  -- Close window for terminals not managed by Snacks
+  vim.cmd("close")
+end
+vim.keymap.set("t", "<C-/>", close_terminal, { desc = "Close Terminal" })
+vim.keymap.set("t", "<C-_>", close_terminal, { desc = "Close Terminal (same as C-/)" })
